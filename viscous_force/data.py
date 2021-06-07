0bb91dbfaabd74e61316381670dce29a9d88c77e
J-Massey <masseyjmo@gmail.com>
2021-06-07 19:29:44 +0100

    Little by little

--- viscous_force/data.py
+++ viscous_force/data.py
@@ -21,20 +21,20 @@ print(device)
 plt.style.use(['science', 'grid'])
 
 data_root = '/home/masseyjmo/Workspace/Lotus/projects/cylinder_dns/sims/res_test/'
-fn = 'd-96'
+fn = 'dis-96'
 pressure = 'fort.10'
 force_file = 'fort.9'
 u_0 = 'profiles'
 
-names = ['t', 'dt', 'px', 'py', 'pz', 'vx', 'vy', 'vz', 'v2x', 'v2y', 'v2z']
+names = ['torch', 'dt', 'px', 'py', 'pz', 'vx', 'vy', 'vz', 'v2x', 'v2y', 'v2z']
 fos = (postproc.io.unpack_flex_forces(os.path.join(data_root, fn, '3D', force_file), names))
 forces_dic = dict(zip(names, fos))
-t_min = min(forces_dic['t'])
-t_max = max(forces_dic['t'])
-t = forces_dic['t']
+t_min = min(forces_dic['torch'])
+t_max = max(forces_dic['torch'])
+t = forces_dic['torch']
 
 profiles = postproc.boundary_layer.ProfileDataset(os.path.join(data_root, fn, '3D'), True)
-rs, u0 = profiles.bl_poincare_limit(single_point=True, position=0.5, length_scale=96, print_res=256, print_len=3)
+rs, u0 = profiles.bl_value(single_point=True, position=0.5, length_scale=96, print_res=256, print_len=3)
 
 
 if __name__ == "__main__":
